b0b2536970d61b978707c29345f429c4bcbdefce
alexzh3 <alexzheng@hotmail.nl>
2025-03-25 22:06:00 +0100

    Added new results from local tests

--- cs4575_project2/dockerfiles/run_images.ps1
+++ cs4575_project2/dockerfiles/run_images.ps1
@@ -3,5 +3,5 @@ Get-ChildItem -Filter "*.Dockerfile" | ForEach-Object {
     $imageName = $_.BaseName  # Extracts the filename without extension
     Write-Host "Running image: $imageName with GPU support"
     # Run the container interactively to display its output
-    docker run --gpus all --volume $PSScriptRoot\dockerfiles:/app:rw --interactive $imageName
+    docker run --gpus all --volume $PSScriptRoot:/app:rw --interactive $imageName
 }
