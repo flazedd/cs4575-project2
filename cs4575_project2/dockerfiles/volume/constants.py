6c233257de611ef290ae164bce30783c80d36c4c
Alex Z <alexzheng@hotmail.nl>
2025-03-30 12:33:06 +0200

    Experminent results, check constants.py for variables

--- cs4575_project2/dockerfiles/volume/constants.py
+++ cs4575_project2/dockerfiles/volume/constants.py
@@ -1,8 +1,8 @@
-TASKS = 23 # Amount of tasks to run
+TASKS = 23 # Amount of tasks to run (23 in total for swe-bench_lite_oracle)
 RESULT_FOLDER = 'results_Alex' # Results folder, change to ur specific custom one
 
 # Parameters for LLM
-MAX_OUTPUT_TOKENS = 8192 # The max amount of output tokens generated
+MAX_OUTPUT_TOKENS = 4096 # The max amount of output tokens generated
 MAX_CONTEXT_WINDOW = 32768 # The total length of the context window (input + output)
 TEMPERATURE = 0.0 # Temperature for most deterministic results (default)
 REPETITION_PENALTY = 1.05 # Sets how strongly to penalize repetitions. (default)
